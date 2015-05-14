5c93a8e74d821ea1b0b8d965f5fb02cd7bb04fa7
Billy Shen <bshen@ibsciss.com>
2015-05-14 19:49:57 +0200

    [MASTER] Add TweenMax to handle animations

--- app/config.js
+++ app/config.js
@@ -5,8 +5,31 @@ require.config({
     "template": "../bower_components/lodash-template-loader/loader",
     "jquery": "../bower_components/jquery/dist/jquery",
     "backbone": "../bower_components/backbone/backbone",
-    "react": "../bower_components/react/react"
+    "react": "../bower_components/react/react",
+    "gsap": "../bower_components/gsap/src/minified/TweenMax.min"
   },
+
+    shim: {
+        'jquery' : {
+            exports: '$'
+        },
+        'lodash' : {
+            exports: '_'
+        },
+        'backbone': {
+            deps: ['jquery', 'lodash'],
+            exports: 'Backbone'
+        },
+        'gsap' : {
+            exports: 'TweenMax'
+        },
+        'mapbox' : {
+            exports: 'L'
+        },
+        'slider' : {
+            exports: 'S'
+        }
+    },
 
   deps: ["main"]
 });
